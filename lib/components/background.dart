9fe392b97142683c41ae52aa01f070307cf9e93f
Dih <djalma2468@hotmail.com>
2020-02-05 21:19:33 -0300

    - Removed some unnecessary comments.

--- lib/components/background.dart
+++ lib/components/background.dart
@@ -6,15 +6,12 @@ import 'package:flame/sprite.dart';
 class Background extends Component {
 
   Size size;
-
   Sprite sprite;
-  //Rect bgRect;
   int speed;
   List<Rect> rects = [];
 
   Background({this.size, this.sprite, this.speed}){
 
-    //bgRect = Rect.fromLTWH(0, 0, size.width, size.height);
     rects.add(Rect.fromLTWH(0, 0, size.width, size.height));
     rects.add(Rect.fromLTWH(size.width, 0, size.width +1, size.height));
 
@@ -35,20 +32,17 @@ class Background extends Component {
       for(int i =0; i < rects.length; i++){
         rects[i] = rects[i].translate(speed * dt, 0);
 
-        //print("RIGHT: ${rects[0].right}");
-        //print("METADE DO WIDTH: ${size.width / 2}");
-
         if(rects[i].right <= -1){
 
+          // Adiciona o background na lista
           rects.add(Rect.fromLTWH(size.width + 1, 0, size.width +1, size.height));
-          //print("ADICIONOU NOVO BACKGROUND!");
 
         }
 
         if(rects[i].right <= -1){
 
+          // Remove o background da lista ao sair da tela
           rects.remove(rects[i]);
-          //print("REMOVEU BACKGROUND: $i");
 
         }
 
